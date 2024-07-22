4954abd17f8bdfd222690b640d41a0b37e310a18
derekyu9 <derekyu9@yahoo.com>
2024-07-22 15:29:09 +0100

    add map template

--- generate.py
+++ generate.py
@@ -2,18 +2,20 @@ from jinja2 import Environment, FileSystemLoader
 import os
 import json
 
+# ----------------------------------- helper funcs ----------------------------------- #
 def convert_snakecase(value):
     return value.replace(' ', '_').lower()
 
 def map_exists(trek):
-	filename = "maps/" + convert_snakecase(trek) + ".html"
-	return os.path.exists(filename)
+	return "map" in trek.keys()
 
 def get_images(trek):
 	directory = "images/" + convert_snakecase(trek)
 	images = [image for image in os.listdir(directory) if not image.startswith("thumbnail")]
 	return sorted(images, key=lambda x: int(os.path.splitext(x)[0]))
- 
+
+# # ---------------------script to generate static site html -------------------------- #
+
 treks_file = "treks.json"
 
 with open(treks_file, 'r') as file:
@@ -32,11 +34,19 @@ with open(filename, 'w+') as f:
         treks = [t["name"] for t in treks]
     ))
 
+#render maps
+for trek in treks:
+    if map_exists(trek):
+        template = env.get_template('map.html')
+        filename = f"maps/{trek['name'].lower().replace(' ', '_')}.html"
+        with open(filename, 'w+') as f:
+            f.write(template.render(trek=trek))
+
 #render albums
 for trek in treks:
     template = env.get_template('album.html')
     filename = f"albums/{trek['name'].lower().replace(' ', '_')}.html"
     with open(filename, 'w+') as f:
         f.write(template.render(trek=trek, 
-        						render_map=map_exists(trek['name']),
+        						render_map=map_exists(trek),
         						images=get_images(trek['name'])))
